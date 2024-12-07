20df36dc5ef1a36478c4c7916916a03f585e2512
bbantier <65014410+bbantier@users.noreply.github.com>
2024-12-07 11:19:31 +0100

    set gameboard as 1D array

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 const gameboard = (function () {
   const gameboard = [
-    // Explore the possiblity to use only one array
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
   ];
 
-  function placeMarker(marker, x, y) {
-    gameboard[x - 1][y - 1] = marker;
+  function placeMarker(marker, position) {
+    gameboard[position - 1] = marker;
   }
 
-  return { gameboard, placeMarker };
+  function getGameboardState() {
+    console.log(gameboard);
+  }
+
+  return { placeMarker, getGameboardState };
 })();
 
 const createPlayer = function (playerNumber) {
-  const playerName = prompt();
+  const playerName = `Player ${playerNumber}`;
   const playerMarker = playerNumber === 1 ? "x" : "o";
+
   return { playerName, playerMarker };
 };
 
-const playGame = function () {
+const game = (function () {
   const playerOne = createPlayer(1);
   const playerTwo = createPlayer(2);
-
-  function playTurn(x, y) {
-    let turn = 1;
-    const currentPlayer = turn % 2 === 0 ? playerOne : playerTwo;
-    x = prompt();
-    y = prompt();
-    gameboard.placeMarker(currentPlayer.playerMarker, x, y);
-    turn++;
-  }
-};
+})();
