549cd953eaaffb9dc32e24c145fd769acfd5b3ec
bbantier <65014410+bbantier@users.noreply.github.com>
2024-12-07 12:02:50 +0100

    start building game logic

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 const gameboard = (function () {
-  const gameboard = [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ];
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
 
   function placeMarker(marker, position) {
     gameboard[position - 1] = marker;
@@ -17,6 +7,7 @@ const gameboard = (function () {
 
   function getGameboardState() {
     console.log(gameboard);
+    return gameboard;
   }
 
   return { placeMarker, getGameboardState };
@@ -29,7 +20,36 @@ const createPlayer = function (playerNumber) {
   return { playerName, playerMarker };
 };
 
-const game = (function () {
+const game = function () {
   const playerOne = createPlayer(1);
   const playerTwo = createPlayer(2);
-})();
+  let currentPlayer = playerOne;
+
+  function switchPlayer() {
+    currentPlayer === playerOne ? playerTwo : playerOne;
+  }
+
+  function playTurn(position) {
+    position = prompt();
+    gameboard.placeMarker(currentPlayer.marker, position);
+    evaluateGame(currentPlayer.marker);
+  }
+
+  function evaluateGame(marker) {
+    gameboard.getGameboardState().forEach((cell, index, arr) => {
+      if (index === 0 || index % 3 === 0) {
+        if (
+          cell === marker &&
+          arr[index + 1] === marker &&
+          arr[index + 2] === marker
+        ) {
+          console.log(currentPlayer.playerName + " wins!");
+          return;
+        } else {
+          switchPlayer();
+          playTurn();
+        }
+      }
+    });
+  }
+};
